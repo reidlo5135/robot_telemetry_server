afc79d035427ac7f75ee5be1092c2977d73e8ad3
reidlo5135 <reidlo4215@gmail.com>
2026-06-22 15:27:47 +0900

    feat: add telemetry topic subscriptions

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use robot_telemetry_server::RobotTelemetryServerNode;
 fn main() -> Result<()> {
     let context: Context = Context::default_from_env()?;
     let mut executor: Executor = context.create_basic_executor();
-    let _node: RobotTelemetryServerNode =
-        RobotTelemetryServerNode::new(&executor, "robot_telemetry_server")?;
+    let _node: RobotTelemetryServerNode = RobotTelemetryServerNode::new(&executor, "robot_telemetry_server")?;
     println!("Robot Telemetry Server Node has started.");
     executor.spin(SpinOptions::default()).first_error()?;
     Ok(())
--- src/node.rs
+++ src/node.rs
@@ -13,7 +13,19 @@ pub struct RobotTelemetryServerNode {
     odom_subscription: WorkerSubscription<nav_msgs::msg::Odometry, TelemetryState>,
 
     #[allow(dead_code)]
+    imu_subscription: WorkerSubscription<sensor_msgs::msg::Imu, TelemetryState>,
+
+    #[allow(dead_code)]
     scan_subscription: WorkerSubscription<sensor_msgs::msg::LaserScan, TelemetryState>,
+
+    #[allow(dead_code)]
+    twist_subscription: WorkerSubscription<geometry_msgs::msg::Twist, TelemetryState>,
+
+    #[allow(dead_code)]
+    battery_state_subscription: WorkerSubscription<sensor_msgs::msg::BatteryState, TelemetryState>,
+
+    #[allow(dead_code)]
+    tf_subscription: WorkerSubscription<tf2_msgs::msg::TFMessage, TelemetryState>,
 }
 
 impl RobotTelemetryServerNode {
@@ -42,6 +54,23 @@ impl RobotTelemetryServerNode {
             },
         )?;
 
+        let mut imu_subscription_opts: SubscriptionOptions<'_> = SubscriptionOptions::new("/imu");
+        imu_subscription_opts.qos = imu_subscription_opts.qos.reliable().keep_last(10);
+
+        let imu_subscription: Arc<
+            SubscriptionState<sensor_msgs::msg::Imu, Arc<WorkerState<TelemetryState>>>,
+        > = worker.create_subscription(
+            imu_subscription_opts,
+            |state: &mut TelemetryState, msg: sensor_msgs::msg::Imu| {
+                rclrs::log_info!(
+                    state.node.logger(),
+                    "Received /imu message linear_acceleration.x={:.3}, angular_velocity.z={:.3}",
+                    msg.linear_acceleration.x,
+                    msg.angular_velocity.z
+                );
+            },
+        )?;
+
         let mut scan_subscription_opts: SubscriptionOptions<'_> = SubscriptionOptions::new("/scan");
         scan_subscription_opts.qos = scan_subscription_opts.qos.best_effort().keep_last(5);
 
@@ -58,10 +87,64 @@ impl RobotTelemetryServerNode {
             },
         )?;
 
+        let mut twist_subscription_opts: SubscriptionOptions<'_> = SubscriptionOptions::new("/cmd_vel");
+        twist_subscription_opts.qos = twist_subscription_opts.qos.reliable().keep_last(10);
+
+        let twist_subscription: Arc<
+            SubscriptionState<geometry_msgs::msg::Twist, Arc<WorkerState<TelemetryState>>>,
+        > = worker.create_subscription(
+            twist_subscription_opts,
+            |state: &mut TelemetryState, msg: geometry_msgs::msg::Twist| {
+                rclrs::log_info!(
+                    state.node.logger(),
+                    "Received /cmd_vel message linear.x={:.3}, angular.z={:.3}",
+                    msg.linear.x,
+                    msg.angular.z
+                );
+            },
+        )?;
+
+        let mut battery_state_subscription_opts: SubscriptionOptions<'_> = SubscriptionOptions::new("/battery_state");
+        battery_state_subscription_opts.qos = battery_state_subscription_opts.qos.reliable().keep_last(10);
+
+        let battery_state_subscription: Arc<
+            SubscriptionState<sensor_msgs::msg::BatteryState, Arc<WorkerState<TelemetryState>>>,
+        > = worker.create_subscription(
+            battery_state_subscription_opts,
+            |state: &mut TelemetryState, msg: sensor_msgs::msg::BatteryState| {
+                rclrs::log_info!(
+                    state.node.logger(),
+                    "Received /battery_state message voltage={:.3}, current={:.3}",
+                    msg.voltage,
+                    msg.current
+                );
+            },
+        )?;
+
+        let mut tf_subscription_opts: SubscriptionOptions<'_> = SubscriptionOptions::new("/tf");
+        tf_subscription_opts.qos = tf_subscription_opts.qos.reliable().keep_last(100);
+
+        let tf_subscription: Arc<
+            SubscriptionState<tf2_msgs::msg::TFMessage, Arc<WorkerState<TelemetryState>>>,
+        > = worker.create_subscription(
+            tf_subscription_opts,
+            |state: &mut TelemetryState, msg: tf2_msgs::msg::TFMessage| {
+                rclrs::log_info!(
+                    state.node.logger(),
+                    "Received /tf message with {} transforms",
+                    msg.transforms.len()
+                );
+            },
+        )?;
+
         Ok(Self {
             node,
             odom_subscription,
+            imu_subscription,
             scan_subscription,
+            twist_subscription,
+            battery_state_subscription,
+            tf_subscription,
         })
     }
 }
